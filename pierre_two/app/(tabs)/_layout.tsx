e6b6004ac670c0347d2d57ef720e1ca01723ea41
root-kawaii <teo.regge.99@gmail.com>
2026-03-30 17:29:29 +0200

    Revert "test: star icon to verify OTA, inline marzipano"
    This reverts commit 4be0e13f145f1574869b4b562105abb69ad34608.

--- pierre_two/app/(tabs)/_layout.tsx
+++ pierre_two/app/(tabs)/_layout.tsx
@@ -23,7 +23,7 @@ export default function TabLayout() {
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="star.fill" color={color} />,
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
       <Tabs.Screen
