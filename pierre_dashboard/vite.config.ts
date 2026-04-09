3724c378d3bcd7a8da7db121d12c2740bf03a621
Valerio Colombo <colombodevdesign@gmail.com>
2026-04-09 23:53:27 +0200

    force ssl on dev build only

--- pierre_dashboard/vite.config.ts
+++ pierre_dashboard/vite.config.ts
@@ -3,10 +3,15 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 
+const isDev = process.env.NODE_ENV !== 'production'
+
 export default defineConfig({
-  plugins: [react(), tailwindcss(), basicSsl()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    isDev ? basicSsl() : null,
+  ],
   server: {
-    https: true,
     host: true,
   },
 })
